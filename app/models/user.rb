5a426cb6554ace56cd8b690d932b4d2027145b45
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-03-30 08:44:54 +0000

    応用課題4完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,35 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  #フォローしている関連付け
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  #フォローされている関連付け
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  #フォローしているユーザー取得
+  has_many :followings, through: :active_relationships, source: :followed
+  #フォロワー取得
+  has_many :followers, through: :passive_relationships, source: :follower
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
   
+  #指定したユーザーをフォローする
+  def follow(user_id)
+    active_relationships.create(followed_id: user_id)
+  end
+
+  #指定したユーザーのフォローを解除する
+  def unfollow(user_id)
+    active_relationships.find_by(followed_id: user_id).destroy
+  end
+
+  #指定したユーザーをフォローしているかの判断
+  def following?(user)
+    followings.include?(user)
+  end
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'

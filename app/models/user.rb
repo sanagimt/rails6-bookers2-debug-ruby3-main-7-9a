274c16fc447556f4f354ecf2817f59a130c61f8e
sanagimt <180413622+sanagimt@users.noreply.github.com>
2025-04-09 11:39:47 +0000

    応用課題9a完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
   has_many :rooms, through: :user_rooms
+  has_many :view_counts, dependent: :destroy
   
   #フォローしている関連付け
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
